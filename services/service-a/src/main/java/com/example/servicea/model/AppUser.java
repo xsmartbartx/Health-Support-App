f2a8cdd45657c65051797ed631583428d1e97447
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-08-16 13:23:15 +0200

    app user

---
+++ services/service-a/src/main/java/com/example/servicea/model/AppUser.java
@@ -0,0 +1,33 @@
+package com.example.servicea.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public AppUser() {}
+
+    public AppUser(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
