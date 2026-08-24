a91f05768e09ec619f68296fd1bdd24855d7e106
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-08-24 21:54:16 +0200

    gradle build fix

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,3 @@
 @echo off
 REM Minimal gradle-wrapper for Windows using Docker
-docker run --rm -v "%cd%":/home/gradle/project -w /home/gradle/project gradle:8.4.1-jdk17 gradle %*
+docker run --rm -v "%cd%\services\service-a":/home/gradle/project -w /home/gradle/project gradle:8-jdk17 gradle %*
